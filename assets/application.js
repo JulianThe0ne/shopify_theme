2678ba6ee8392aa6200752534a5d60f5033ac67b
Julien Afonso <ju.afonso61@gmail.com>
2023-06-14 11:40:46 +0900

    connect-test

--- assets/application.js
+++ assets/application.js
@@ -1,9 +1,1 @@
 // Put your application javascript here
-// const addToCartForms = document.querySelectorAll('form[action="/cart/add"]');
-const addToCartForms = document.querySelectorAll('form[action="/cart/add]');
-
-addToCartForms.forEach((form) => {
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  });
-});
